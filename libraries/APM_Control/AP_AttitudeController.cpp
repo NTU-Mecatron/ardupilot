e715051980fdf42eda7f609b5479f492a9e70a2f
lucvt001 <luc001@e.ntu.edu.sg>
2026-09-07 22:31:07 +0800

    Minor change

--- libraries/APM_Control/AP_AttitudeController.cpp
+++ libraries/APM_Control/AP_AttitudeController.cpp
@@ -1,21 +1,3 @@
-/*
-   This program is free software: you can redistribute it and/or modify
-   it under the terms of the GNU General Public License as published by
-   the Free Software Foundation, either version 3 of the License, or
-   (at your option) any later version.
-
-   This program is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-   GNU General Public License for more details.
-
-   You should have received a copy of the GNU General Public License
-   along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
-
-//	Originally by Jon Challinger, modified by Paul Riseborough (roll/pitch/yaw controllers)
-//  Merged into a single axis-independent controller for torpedo-shaped AUVs
-
 #include <AP_HAL/AP_HAL.h>
 #include "AP_AttitudeController.h"
 #include <AP_AHRS/AP_AHRS.h>
