173944aebfbdc331258ee5ecb1bc014b32329169
lucvt001 <luc001@e.ntu.edu.sg>
2026-09-12 16:26:10 +0800

    Replace gains mixing with fins mixing

--- ArduPlane/Parameters.cpp
+++ ArduPlane/Parameters.cpp
@@ -591,12 +591,9 @@ const AP_Param::Info Plane::var_info[] = {
     // @User: Advanced
     GSCALAR(ground_steer_dps,         "GROUND_STEER_DPS",  90),
 
-    // @Param: MIXING_GAIN
-    // @DisplayName: Mixing Gain
-    // @Description: The gain for the Vtail and elevon output mixers. The default is 0.5, which ensures that the mixer doesn't saturate, allowing both input channels to go to extremes while retaining control over the output. Hardware mixers often have a 1.0 gain, which gives more servo throw, but can saturate. If you don't have enough throw on your servos with VTAIL_OUTPUT or ELEVON_OUTPUT enabled then you can raise the gain using MIXING_GAIN. The mixer allows outputs in the range 900 to 2100 microseconds.
-    // @Range: 0.5 1.2
-    // @User: Standard
-    GSCALAR(mixing_gain,            "MIXING_GAIN",    0.5f),
+    // @Group: FINS_
+    // @Path: fins_mixing.cpp
+    GOBJECT(fins_mixing,      "FINS_",  FinsMixing),
 
     // @Param: RUDDER_ONLY
     // @DisplayName: Rudder only aircraft
