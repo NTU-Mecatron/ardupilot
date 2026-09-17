5478758e1b7dbb7d4c7f877e3c113c40dc9215d2
lucvt001 <luc001@e.ntu.edu.sg>
2026-09-17 23:01:43 +0800

    Updated takeoff logic to make it work

--- ArduPlane/mode_takeoff.cpp
+++ ArduPlane/mode_takeoff.cpp
@@ -53,7 +53,7 @@ ModeTakeoff::ModeTakeoff() :
 bool ModeTakeoff::_enter()
 {
     // Do not enter takeoff if we are already running
-    const uint16_t altitude = plane.relative_ground_altitude(false,true);
+    const float altitude = plane.adjusted_altitude_cm() * 0.01f;
     if (altitude < -1.0 && (millis() - plane.started_flying_ms > 5000U)) {
         return false;   // Luc_TODO: check what happen if return false
     }
@@ -61,6 +61,16 @@ bool ModeTakeoff::_enter()
     initial_heading_cd = -1;
     has_logged_reached_takeoff_speed = false;
     has_logged_reached_target_alt = false;
+
+    // Set target speed
+    plane.target_speed_ms = takeoff_speed;
+
+    // Set target altitude
+    const float direction = degrees(ahrs.get_yaw());
+    plane.next_WP_loc = plane.current_loc;
+    plane.next_WP_loc.alt += target_alt*100.0;
+    plane.next_WP_loc.offset_bearing(direction, 1000);
+
     return true;
 }
 
@@ -70,7 +80,7 @@ void ModeTakeoff::update()
     plane.calc_throttle();
 
     // If we are too slow, pitch and yaw will be heavily affected by waves so it is better to hardcode elevator and rudder
-    if (current_speed < takeoff_speed) 
+    if (plane.get_forward_speed() < takeoff_speed * 0.9) 
     {
         SRV_Channels::set_output_scaled(SRV_Channel::k_elevator, surface_elevator * 100);
         SRV_Channels::set_output_scaled(SRV_Channel::k_rudder, 0.0);
@@ -78,7 +88,7 @@ void ModeTakeoff::update()
     else    // Run inner PID loops as per usual
     {
         plane.calc_nav_pitch();
-        plane.calc_nav_yaw_ground();    // Luc_TODO: implement a general calc_nav_yaw
+        plane.calc_nav_yaw_rate();
 
         if (!has_logged_reached_takeoff_speed) {
             has_logged_reached_takeoff_speed = true;
@@ -88,15 +98,14 @@ void ModeTakeoff::update()
     }
 
     // Check if reached target alt (which should be a negative number)
-    const float altitude = plane.relative_ground_altitude(false,true);
+    const float altitude = plane.adjusted_altitude_cm() * 0.01f;
     if (-altitude <= -target_alt) {
         plane.set_flight_stage(AP_FixedWing::FlightStage::TAKEOFF);
     } else {
         plane.set_flight_stage(AP_FixedWing::FlightStage::NORMAL);
         if (!has_logged_reached_target_alt) {
             has_logged_reached_target_alt = true;
-            const float alt = target_alt;
-            gcs().send_text(MAV_SEVERITY_INFO, "Current alt %.1f m, Reached target altitude of %.1f m", altitude, alt);
+            gcs().send_text(MAV_SEVERITY_INFO, "Current alt %.1f m, Reached target altitude of %.1f m", altitude, target_alt.get());
         }
     }
 }
@@ -104,7 +113,7 @@ void ModeTakeoff::update()
 void ModeTakeoff::run()
 {
     // When not enough speed, let the update() method handle hardcode control of elevator and rudder
-    if (current_speed >= takeoff_speed)
+    if (plane.get_forward_speed() >= takeoff_speed * 0.9)
     {
         // Normal flight, run base class
         Mode::run();
@@ -113,15 +122,6 @@ void ModeTakeoff::run()
 
 void ModeTakeoff::navigate()
 {
-    // Position and velocity check
-    bool have_valid_position_and_home = plane.current_loc.initialised() && AP::ahrs().home_is_set();
-    bool have_speed = plane.speedController.get_forward_speed(current_speed);
-    if (!have_valid_position_and_home || !have_speed) {
-        plane.arming.disarm(AP_Arming::Method::EKFFAILSAFE, false);
-        plane.set_mode(Mode::Number::MANUAL, ModeReason::EKF_FAILSAFE);
-        return;
-    }
-
     // Check for optional timeout
     if (plane.g2.takeoff_timeout > 0) 
     {
@@ -134,10 +134,6 @@ void ModeTakeoff::navigate()
             return;
         }
     }
-    
-
-    plane.speedController.set_target_speed(takeoff_speed * 1.1f);   // Set a slightly higher target speed for margin
-    plane.alt_pitch_controller.set_target_altitude(target_alt * 100);   // Always set target depth, but update() method only pitch down if has reached sufficient speed
 
     if (takeoff_in_circle) {
         // Luc_TODO: update loiter with changing depth, or separate the axes
